bfeb90827c820dc44bc698bc7829f71292d59584
Richard Littauer <richard.littauer@gmail.com>
2014-12-10 15:27:41 -0800

    Syntax errors fixed

--- cli-index.js
+++ cli-index.js
@@ -10,18 +10,16 @@ var argv = require('minimist')(process.argv.slice(2),  {
         g: 'groups',
         h: 'help'
     },
-    boolean: ['e', 'd', 'm', 'g']
+    boolean: ['e', 'd', 'm', 'g', 'h']
 });
-
-console.log(argv);
 
 if (argv.h) {
 	console.error(
-	"Usage: " + process.argv[1] + " <doi> \n\
-	Options: \n\
-	-e, --exact	Find an exact match \n\
-	-d, --declared 	Find a DOI with a 'doi:' prefix\n\
-	-m, --match	Find all matches within the given string");
+	"Usage: " + process.argv[1] + " <doi>\n" +
+	"Options: \n" +
+	"-e, --exact	Find an exact match \n" +
+	"-d, --declared 	Find a DOI with a 'doi:' prefix\n" +
+	"-m, --match	Find all matches within the given string");
 	process.exit(-1)
 }
 
@@ -29,17 +27,19 @@ var doi = (argv.doi || argv._[0])
 
 if (argv.m) {
 	console.log(doi.match(doiRegex()))
+	process.exit(-1)
 } else if (argv.g) {
 	console.log(doiRegex.groups(doi));
+	process.exit(-1)
+}
+
+if (argv.e && argv.d) {
+	console.log('Is this a declared DOI',
+		doiRegex.declared({exact: true}).test(doi))
+} else if (argv.e && !argv.d) {
+	console.log('Is this a DOI?', doiRegex({exact: true}).test(doi))
+} else if (!argv.e && argv.d) {
+	console.log('Is the DOI declared?', doiRegex.declared().test(doi))
 } else {
-	if (argv.e && argv.d) {
-		console.log('Is this a declared DOI',
-			doiRegex.declared({exact: true}).test(doi))
-	} else if (argv.e && !argv.d) {
-		console.log('Is this a DOI?', doiRegex({exact: true}).test(doi))
-	} else if (!argv.e && argv.d) {
-		console.log('Is the DOI declared?', doiRegex.declared().test(doi))
-	} else {
-		console.log('Does a DOI exist?', doiRegex().test(doi))
-	}
+	console.log('Does a DOI exist?', doiRegex().test(doi))
 }
